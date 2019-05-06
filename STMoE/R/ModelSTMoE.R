0daa50acce3c7e5b675f3900bb73d4741a96120d
Marius <marius.bartcus@gmail.com>
2019-05-06 13:45:20 +0200

    ModelSTMoE

---
+++ STMoE/R/ModelSTMoE.R
@@ -0,0 +1,32 @@
+source("R/FData.R")
+
+ModelSTMoE <- setRefClass(
+  "ModelSTMoE",
+  contains = "FData",
+  # Define the fields
+  fields = list(
+    K = "numeric",
+    # number of regimes
+    p = "numeric",
+    # dimension of beta (order of polynomial regression)
+    q = "numeric",
+    # dimension of w (order of logistic regression)
+    nu = "numeric" # degree of freedom
+  )
+)
+
+ModelSTMoE <- function(fData, K, p, q) {
+  nu <<- (p + q + 3) * K - (q + 1)
+
+  new(
+    "ModelSTMoE",
+    Y = fData$Y,
+    X = fData$X,
+    m = fData$m,
+    n = fData$n,
+    K = K,
+    p = p,
+    q = q,
+    nu = nu
+  )
+}
